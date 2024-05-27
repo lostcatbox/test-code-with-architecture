9d6a73e20c70c5d0de245bf1a8133242d0c30e39
lostcatbox <lostcatbox@gmail.com>
2024-05-27 22:26:38 +0900

    fix: ClockHolder, UUIDHolder 컴포넌트화

--- src/main/java/com/example/demo/common/infrastructure/SystemClockHolder.java
+++ src/main/java/com/example/demo/common/infrastructure/SystemClockHolder.java
@@ -1,9 +1,11 @@
 package com.example.demo.common.infrastructure;
 
 import com.example.demo.common.service.port.ClockHolder;
+import org.springframework.stereotype.Component;
 
 import java.time.Clock;
 
+@Component
 public class SystemClockHolder implements ClockHolder {
     @Override
     public long millis() {
--- src/main/java/com/example/demo/common/infrastructure/SystemUUIDHolder.java
+++ src/main/java/com/example/demo/common/infrastructure/SystemUUIDHolder.java
@@ -1,9 +1,11 @@
 package com.example.demo.common.infrastructure;
 
 import com.example.demo.common.service.port.UUIDHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class SystemUUIDHolder implements UUIDHolder {
     @Override
     public String randomUUID() {
