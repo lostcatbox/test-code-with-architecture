c9c9631b3a9f6c52bc5ab03c9590a85d193065c0
lostcatbox <lostcatbox@gmail.com>
2024-05-28 09:10:55 +0900

    test: UserController 테스트 코드 작성 및 TestUserContainer 생성

--- src/main/java/com/example/demo/user/controller/UserController.java
+++ src/main/java/com/example/demo/user/controller/UserController.java
@@ -1,26 +1,19 @@
 package com.example.demo.user.controller;
 
+import com.example.demo.user.controller.dto.request.UserUpdateDto;
 import com.example.demo.user.controller.dto.response.MyProfileResponse;
 import com.example.demo.user.controller.dto.response.UserResponse;
-import com.example.demo.user.controller.dto.request.UserUpdateDto;
-import com.example.demo.user.service.UserServiceImpl;
+import com.example.demo.user.controller.port.UserService;
 import com.example.demo.user.repository.model.User;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @Tag(name = "유저(users)")
 @RestController
@@ -28,52 +21,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserServiceImpl userServiceImpl;
+    private final UserService userService;
 
     @ResponseStatus
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable long id) {
         return ResponseEntity
-            .ok()
-            .body(UserResponse.from(userServiceImpl.getByIdOrElseThrow(id)));
+                .ok()
+                .body(UserResponse.from(userService.getByIdOrElseThrow(id)));
     }
 
     @GetMapping("/{id}/verify")
     public ResponseEntity<Void> verifyEmail(
-        @PathVariable long id,
-        @RequestParam String certificationCode) {
-        userServiceImpl.verifyEmail(id, certificationCode);
+            @PathVariable long id,
+            @RequestParam String certificationCode) {
+        userService.verifyEmail(id, certificationCode);
         return ResponseEntity.status(HttpStatus.FOUND)
-            .location(URI.create("http://localhost:3000"))
-            .build();
+                .location(URI.create("http://localhost:3000"))
+                .build();
     }
 
     @GetMapping("/me")
     public ResponseEntity<MyProfileResponse> getMyInfo(
-        @Parameter(name = "EMAIL", in = ParameterIn.HEADER)
-        @RequestHeader("EMAIL") String email // 일반적으로 스프링 시큐리티를 사용한다면 UserPrincipal 에서 가져옵니다.
+            @Parameter(name = "EMAIL", in = ParameterIn.HEADER)
+            @RequestHeader("EMAIL") String email // 일반적으로 스프링 시큐리티를 사용한다면 UserPrincipal 에서 가져옵니다.
     ) {
-        User user = userServiceImpl.getByEmail(email);
+        User user = userService.getByEmail(email);
         return ResponseEntity
-            .ok()
-            .body(toMyProfileResponse(user));
+                .ok()
+                .body(toMyProfileResponse(user));
     }
 
     @PutMapping("/me")
     @Parameter(in = ParameterIn.HEADER, name = "EMAIL")
     public ResponseEntity<MyProfileResponse> updateMyInfo(
-        @Parameter(name = "EMAIL", in = ParameterIn.HEADER)
-        @RequestHeader("EMAIL") String email, // 일반적으로 스프링 시큐리티를 사용한다면 UserPrincipal 에서 가져옵니다.
-        @RequestBody UserUpdateDto userUpdateDto
+            @Parameter(name = "EMAIL", in = ParameterIn.HEADER)
+            @RequestHeader("EMAIL") String email, // 일반적으로 스프링 시큐리티를 사용한다면 UserPrincipal 에서 가져옵니다.
+            @RequestBody UserUpdateDto userUpdateDto
     ) {
-        User user = userServiceImpl.getByEmail(email);
-        userServiceImpl.updateUser(user.getId(), userUpdateDto);
-        User response = userServiceImpl.getByEmail(email);
+        User user = userService.getByEmail(email);
+        userService.updateUser(user.getId(), userUpdateDto);
+        User response = userService.getByEmail(email);
         return ResponseEntity
-            .ok()
-            .body(toMyProfileResponse(response));
+                .ok()
+                .body(toMyProfileResponse(response));
     }
-
 
 
     public MyProfileResponse toMyProfileResponse(User user) {
---
+++ src/test/java/com/example/demo/user/controller/UserControllerTest.java
@@ -0,0 +1,70 @@
+package com.example.demo.user.controller;
+
+import com.example.demo.support.container.TestUserContainer;
+import com.example.demo.user.constant.UserStatus;
+import com.example.demo.user.controller.dto.response.UserResponse;
+import com.example.demo.user.repository.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+class UserControllerTest {
+    private final TestUserContainer testUserContainer = new TestUserContainer();
+
+
+    //for test
+    private User activeUser;
+    private User pendingUser;
+
+    @BeforeEach
+    void init() {
+        pendingUser = User.builder()
+                .email("lostcatbox@gmail.com")
+                .address("Seoul")
+                .certificationCode("bbbb")
+                .nickname("lostcatbox")
+                .lastLoginAt(222L)
+                .status(UserStatus.PENDING).build();
+        activeUser = User.builder()
+                .email("lostcatbox@gmail.com")
+                .address("Seoul")
+                .certificationCode("bbbb")
+                .nickname("lostcatbox")
+                .lastLoginAt(222L)
+                .status(UserStatus.ACTIVE).build();
+
+        testUserContainer.getUserRepository().save(pendingUser);
+        testUserContainer.getUserRepository().save(activeUser);
+
+    }
+
+    @Test
+    void getUserById() {
+        ResponseEntity<UserResponse> userById = testUserContainer.getUserController().getUserById(activeUser.getId());
+        UserResponse body = userById.getBody();
+        Assertions.assertAll(
+                () -> body.getEmail().equals(activeUser.getEmail()),
+                () -> body.getId().equals(activeUser.getId()),
+                () -> body.getNickname().equals(activeUser.getNickname()),
+                () -> body.getStatus().equals(activeUser.getStatus()),
+                () -> body.getLastLoginAt().equals(activeUser.getLastLoginAt())
+        );
+    }
+
+    @Test
+    void verifyEmail() {
+    }
+
+    @Test
+    void getMyInfo() {
+    }
+
+    @Test
+    void updateMyInfo() {
+    }
+
+    @Test
+    void toMyProfileResponse() {
+    }
+}
